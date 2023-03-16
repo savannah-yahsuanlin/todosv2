48ceabf9035a0a1bfafe57b546ae0bea007edae6
savannah-yahsuanlin <savannahwang75@gmail.com>
2023-03-16 13:43:05 +0800

    updated

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 require('dotenv').config()
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/todos')
+const conn = new Sequelize(process.env.DATABASE_URL)
 
 module.exports = conn
 
