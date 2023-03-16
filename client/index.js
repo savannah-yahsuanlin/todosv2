d62bfb18009c23a30c078d23d269be23ed885200
savannah-yahsuanlin <savannahwang75@gmail.com>
2023-03-16 21:36:47 +0800

    store/index

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
-import store from '../client/store'
+import store from './store/index'
 
 
 ReactDOM.render(
