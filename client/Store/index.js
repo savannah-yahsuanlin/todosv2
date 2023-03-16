b0acb610113e24ae11aac8479dbaa8f88ca7393d
savannah-yahsuanlin <savannahwang75@gmail.com>
2023-03-16 19:10:29 +0800

    lowercase

--- client/Store/index.js
+++ client/Store/index.js
@@ -1,18 +1,15 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import loggingMiddleware from 'redux-logger';
-import thunk from 'redux-thunk';
-import todosReducer from './todos';
-import todoReducer from './todo';
+import  {createStore, applyMiddleware, combineReducers} from 'redux'
+import loggingMiddleware from 'redux-logger'
+import thunk from 'redux-thunk'
+import todosReducer from './todos'
+import todoReducer from './todo'
 
 const rootReducer = combineReducers({
-  todos: todosReducer,
-  todo: todoReducer
-});
+	todos: todosReducer,
+	todo: todoReducer
+})
 
-const store = createStore(
+export default createStore(
   rootReducer,
   applyMiddleware(thunk, loggingMiddleware)
 );
-
-
-export default store
