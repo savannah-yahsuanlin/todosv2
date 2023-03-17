775556535f19e99de7618e700ed6aed88cebdf06
savannah-yahsuanlin <savannahwang75@gmail.com>
2023-03-17 16:26:04 +0800

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  mode: 'development',
   entry: ['./client/index.js'],
   output: {
     path: __dirname,
     filename: './public/bundle.js'
   },
+  mode: 'development',
   context: __dirname,
   devtool: 'source-map',
   module: {
