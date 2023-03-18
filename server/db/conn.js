ac13263533264f2bdfdb3085bb028d6ebf325e78
savannah-yahsuanlin <savannahwang75@gmail.com>
2023-03-18 21:59:07 +0800

    add localhost back

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 require('dotenv').config()
-const conn = new Sequelize(process.env.DATABASE_URL)
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/todos')
 
 module.exports = conn
 
